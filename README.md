3219ccc3834b2caed1f63f2762484a089668c67c
Kris Lamb <lamb.kris@gmail.com>
2025-11-29 02:41:50 -0500

    got colors swizzled properly and added the esp_logo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # freenove_dev_kit_esp32s3
+
+This is a working example using the espidf to drive the display + touch for this kit
+
+https://www.amazon.ca/dp/B0C2K34N7Q?ref=ppx_yo2ov_dt_b_fed_asin_title
+
+Setup the ST7769 + ft6336u touch display with lvgl
+
+Simple demo to display UI with a button allowing screen rotation
--- main/main.c
+++ main/main.c
@@ -15,35 +15,34 @@
 static const char* TAG = "LVGL_PORT_TEST";
 
 /* LCD size */
-#define EXAMPLE_LCD_H_RES   (240)
-#define EXAMPLE_LCD_V_RES   (320)
+#define LCD_H_RES   (240)
+#define LCD_V_RES   (320)
 
 /* LCD settings */
-#define EXAMPLE_LCD_SPI_NUM         (SPI3_HOST)
-#define EXAMPLE_LCD_PIXEL_CLK_HZ    (40 * 1000 * 1000)
-#define EXAMPLE_LCD_CMD_BITS        (8)
-#define EXAMPLE_LCD_PARAM_BITS      (8)
-#define EXAMPLE_LCD_BITS_PER_PIXEL  (16)
-#define EXAMPLE_LCD_DRAW_BUFF_DOUBLE (1)
-#define EXAMPLE_LCD_DRAW_BUFF_HEIGHT (50)
-// #define EXAMPLE_LCD_BL_ON_LEVEL     (1)
+#define LCD_SPI_NUM         (SPI3_HOST)
+#define LCD_PIXEL_CLK_HZ    (40 * 1000 * 1000)
+#define LCD_CMD_BITS        (8)
+#define LCD_PARAM_BITS      (8)
+#define LCD_BITS_PER_PIXEL  (16)
+#define LCD_DRAW_BUFF_DOUBLE (1)
+#define LCD_DRAW_BUFF_HEIGHT (50)
 
 /* LCD pins */
-#define EXAMPLE_LCD_GPIO_SCLK       (GPIO_NUM_21)
-#define EXAMPLE_LCD_GPIO_MOSI       (GPIO_NUM_20)
-#define EXAMPLE_LCD_GPIO_RST        (GPIO_NUM_NC)
-#define EXAMPLE_LCD_GPIO_DC         (GPIO_NUM_0)
-#define EXAMPLE_LCD_GPIO_CS         (GPIO_NUM_47)
-#define EXAMPLE_LCD_GPIO_BL         (GPIO_NUM_NC)
+#define LCD_GPIO_SCLK       (GPIO_NUM_21)
+#define LCD_GPIO_MOSI       (GPIO_NUM_20)
+#define LCD_GPIO_RST        (GPIO_NUM_NC)
+#define LCD_GPIO_DC         (GPIO_NUM_0)
+#define LCD_GPIO_CS         (GPIO_NUM_47)
+#define LCD_GPIO_BL         (GPIO_NUM_NC)
 
 /* Touch settings */
-#define EXAMPLE_TOUCH_I2C_NUM       (0)
-#define EXAMPLE_TOUCH_I2C_CLK_HZ    (400000)
+#define TOUCH_I2C_NUM       (0)
+#define TOUCH_I2C_CLK_HZ    (400000)
 
 /* LCD touch pins */
-#define EXAMPLE_TOUCH_I2C_SCL       (GPIO_NUM_1)
-#define EXAMPLE_TOUCH_I2C_SDA       (GPIO_NUM_2)
-#define EXAMPLE_TOUCH_GPIO_INT      (GPIO_NUM_NC)
+#define TOUCH_I2C_SCL       (GPIO_NUM_1)
+#define TOUCH_I2C_SDA       (GPIO_NUM_2)
+#define TOUCH_GPIO_INT      (GPIO_NUM_NC)
 
 // LVGL image declare
 LV_IMG_DECLARE(esp_logo)
@@ -64,36 +63,33 @@ static esp_err_t app_lcd_init(void)
     /* LCD initialization */
     ESP_LOGD(TAG, "Initialize SPI bus");
     const spi_bus_config_t buscfg = {
-        .sclk_io_num = EXAMPLE_LCD_GPIO_SCLK,
-        .mosi_io_num = EXAMPLE_LCD_GPIO_MOSI,
+        .sclk_io_num = LCD_GPIO_SCLK,
+        .mosi_io_num = LCD_GPIO_MOSI,
         .miso_io_num = GPIO_NUM_NC,
         .quadwp_io_num = GPIO_NUM_NC,
         .quadhd_io_num = GPIO_NUM_NC,
-        .max_transfer_sz = EXAMPLE_LCD_H_RES * EXAMPLE_LCD_DRAW_BUFF_HEIGHT * sizeof(uint16_t),
+        .max_transfer_sz = LCD_H_RES * LCD_DRAW_BUFF_HEIGHT * sizeof(uint16_t),
     };
-    ESP_RETURN_ON_ERROR(spi_bus_initialize(EXAMPLE_LCD_SPI_NUM, &buscfg, SPI_DMA_CH_AUTO), TAG, "SPI init failed");
+    ESP_RETURN_ON_ERROR(spi_bus_initialize(LCD_SPI_NUM, &buscfg, SPI_DMA_CH_AUTO), TAG, "SPI init failed");
 
     ESP_LOGD(TAG, "Install panel IO");
     const esp_lcd_panel_io_spi_config_t io_config = {
-        .dc_gpio_num = EXAMPLE_LCD_GPIO_DC,
-        .cs_gpio_num = EXAMPLE_LCD_GPIO_CS,
-        .pclk_hz = EXAMPLE_LCD_PIXEL_CLK_HZ,
-        .lcd_cmd_bits = EXAMPLE_LCD_CMD_BITS,
-        .lcd_param_bits = EXAMPLE_LCD_PARAM_BITS,
+        .dc_gpio_num = LCD_GPIO_DC,
+        .cs_gpio_num = LCD_GPIO_CS,
+        .pclk_hz = LCD_PIXEL_CLK_HZ,
+        .lcd_cmd_bits = LCD_CMD_BITS,
+        .lcd_param_bits = LCD_PARAM_BITS,
         .spi_mode = 0,
         .trans_queue_depth = 10,
     };
-    ESP_GOTO_ON_ERROR(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)EXAMPLE_LCD_SPI_NUM, &io_config, &lcd_io), err, TAG, "New panel IO failed");
+    ESP_GOTO_ON_ERROR(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_SPI_NUM, &io_config, &lcd_io), err, TAG, "New panel IO failed");
 
     ESP_LOGD(TAG, "Install LCD driver");
     const esp_lcd_panel_dev_config_t panel_config = {
-        .reset_gpio_num = EXAMPLE_LCD_GPIO_RST,
-#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(6, 0, 0)
-        .rgb_endian = LCD_RGB_ENDIAN_BGR,
-#else
-        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_BGR,
-#endif
-        .bits_per_pixel = EXAMPLE_LCD_BITS_PER_PIXEL,
+        .reset_gpio_num = LCD_GPIO_RST,
+        .data_endian = LCD_RGB_DATA_ENDIAN_LITTLE,
+        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,
+        .bits_per_pixel = LCD_BITS_PER_PIXEL,
     };
     ESP_GOTO_ON_ERROR(esp_lcd_new_panel_st7789(lcd_io, &panel_config, &lcd_panel), err, TAG, "New panel failed");
 
@@ -101,10 +97,6 @@ static esp_err_t app_lcd_init(void)
     esp_lcd_panel_init(lcd_panel);
     esp_lcd_panel_mirror(lcd_panel, true, true);
     esp_lcd_panel_disp_on_off(lcd_panel, true);
-
-    /* LCD backlight on */
-    // ESP_ERROR_CHECK(gpio_set_level(EXAMPLE_LCD_GPIO_BL, EXAMPLE_LCD_BL_ON_LEVEL));
-    // Backlight is wired to VCC
 
     return ret;
 
@@ -115,7 +107,7 @@ err:
     if (lcd_io) {
         esp_lcd_panel_io_del(lcd_io);
     }
-    spi_bus_free(EXAMPLE_LCD_SPI_NUM);
+    spi_bus_free(LCD_SPI_NUM);
     return ret;
 }
 
@@ -124,19 +116,19 @@ static esp_err_t app_touch_init(void)
     /* Initilize I2C */
     i2c_master_bus_handle_t i2c_handle = NULL;
     const i2c_master_bus_config_t i2c_config = {
-        .i2c_port = EXAMPLE_TOUCH_I2C_NUM,
-        .sda_io_num = EXAMPLE_TOUCH_I2C_SDA,
-        .scl_io_num = EXAMPLE_TOUCH_I2C_SCL,
+        .i2c_port = TOUCH_I2C_NUM,
+        .sda_io_num = TOUCH_I2C_SDA,
+        .scl_io_num = TOUCH_I2C_SCL,
         .clk_source = I2C_CLK_SRC_DEFAULT,
     };
     ESP_RETURN_ON_ERROR(i2c_new_master_bus(&i2c_config, &i2c_handle), TAG, "");
 
     /* Initialize touch HW */
     const esp_lcd_touch_config_t tp_cfg = {
-        .x_max = EXAMPLE_LCD_H_RES,
-        .y_max = EXAMPLE_LCD_V_RES,
+        .x_max = LCD_H_RES,
+        .y_max = LCD_V_RES,
         .rst_gpio_num = GPIO_NUM_NC, // Shared with LCD reset
-        .int_gpio_num = EXAMPLE_TOUCH_GPIO_INT,
+        .int_gpio_num = TOUCH_GPIO_INT,
         .levels = {
             .reset = 0,
             .interrupt = 0,
@@ -149,7 +141,7 @@ static esp_err_t app_touch_init(void)
     };
     esp_lcd_panel_io_handle_t tp_io_handle = NULL;
     esp_lcd_panel_io_i2c_config_t tp_io_config = ESP_LCD_TOUCH_IO_I2C_FT6x36_CONFIG();
-    tp_io_config.scl_speed_hz = EXAMPLE_TOUCH_I2C_CLK_HZ;
+    tp_io_config.scl_speed_hz = TOUCH_I2C_CLK_HZ;
     ESP_RETURN_ON_ERROR(esp_lcd_new_panel_io_i2c(i2c_handle, &tp_io_config, &tp_io_handle), TAG, "");
 
     return esp_lcd_touch_new_i2c_ft6x36(tp_io_handle, &tp_cfg, &touch_handle);
@@ -172,10 +164,10 @@ static esp_err_t app_lvgl_init(void)
     const lvgl_port_display_cfg_t disp_cfg = {
         .io_handle = lcd_io,
         .panel_handle = lcd_panel,
-        .buffer_size = EXAMPLE_LCD_H_RES * EXAMPLE_LCD_DRAW_BUFF_HEIGHT,
-        .double_buffer = EXAMPLE_LCD_DRAW_BUFF_DOUBLE,
-        .hres = EXAMPLE_LCD_H_RES,
-        .vres = EXAMPLE_LCD_V_RES,
+        .buffer_size = LCD_H_RES * LCD_DRAW_BUFF_HEIGHT,
+        .double_buffer = LCD_DRAW_BUFF_DOUBLE,
+        .hres = LCD_H_RES,
+        .vres = LCD_V_RES,
         .monochrome = false,
 #if LVGL_VERSION_MAJOR >= 9
         .color_format = LV_COLOR_FORMAT_RGB565,
@@ -226,17 +218,17 @@ static void app_main_display(void)
     /* Your LVGL objects code here .... */
 
     /* Create image */
-    // lv_obj_t *img_logo = lv_img_create(scr);
-    // lv_img_set_src(img_logo, &esp_logo);
-    // lv_obj_align(img_logo, LV_ALIGN_TOP_MID, 0, 20);
+    lv_obj_t *img_logo = lv_img_create(scr);
+    lv_img_set_src(img_logo, &esp_logo);
+    lv_obj_align(img_logo, LV_ALIGN_TOP_MID, 0, 20);
 
     /* Label */
     lv_obj_t *label = lv_label_create(scr);
-    lv_obj_set_width(label, EXAMPLE_LCD_H_RES);
+    lv_obj_set_width(label, LCD_H_RES);
     lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0);
 #if LVGL_VERSION_MAJOR == 8
     lv_label_set_recolor(label, true);
-    lv_label_set_text(label, "#FF0000 "LV_SYMBOL_BELL" Hello world Espressif and LVGL "LV_SYMBOL_BELL"#\n#FF9400 "LV_SYMBOL_WARNING" For simplier initialization, use BSP "LV_SYMBOL_WARNING" #");
+    lv_label_set_text(label, "#FF0000 "LV_SYMBOL_BELL" Hello world"LV_SYMBOL_BELL"\n""#00FF00 "LV_SYMBOL_WARNING"Having Fun!"LV_SYMBOL_WARNING"\n""#0000FF Blue text #");
 #else
     lv_label_set_text(label, LV_SYMBOL_BELL" Hello world Espressif and LVGL "LV_SYMBOL_BELL"\n "LV_SYMBOL_WARNING" For simplier initialization, use BSP "LV_SYMBOL_WARNING);
 #endif
